1c1a0dd66128a31dfd9f29a26cba36bd0d528403
Jasperredis <jasperredisispublic@gmail.com>
2026-07-30 22:36:01 -0500

    Add how to play screen, add themes for fun, change CSS, relicense, write an update, and use a JS file.

---
+++ look.js
@@ -0,0 +1,6 @@
+function toggleFont() { document.documentElement.classList.toggle("easy-font") }
+const THEMES = ['light', 'sand', 'ocean', 'watermelon', 'dark', 'demo', 'original'];
+function setTheme(theme) {
+	document.documentElement.classList.remove(...THEMES);
+	document.documentElement.classList.add(theme);
+}
